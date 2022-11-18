6f928f62accff1ed0e00a66f55ab1401cb8179a3
olzh <112284382+oliviaazhu@users.noreply.github.com>
2022-11-18 14:16:41 -0700

    Updated main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,22 +9,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className='title'>Congratulations! You've set up your webapp!</h1>
-
-        <p className='description'>
-          Get started by editing <code>src/pages/index.js</code> to be your
-          project home page.
-        </p>
-        <p className='description2'>
-          Next, each team member will create their own about me file in the
-          pages directory:{' '}
-        </p>
-        <code>src/page/teamMemberName.js</code>
+        <h1 className='title'>Olivia and Estelle's Websites</h1>
 
         <div className='grid'>
           <a href='/ronBurgundy' className='card'>
-            <h3>Example About Me Page &rarr;</h3>
-            <p>Click here to navigate to an example page</p>
+            <h3>Estelle's Page &rarr;</h3>
+            <p>More info on Estelle!</p>
           </a>
           <a
             href='/oliviaZhu'
@@ -33,24 +23,6 @@ export default function Home() {
             <p>
               More info on Olivia!
             </p>
-          </a>
-          <a
-            href='https://nextjs.org/learn/foundations/about-nextjs'
-            className='card'>
-            <h3>Next.js&rarr;</h3>
-            <p>Click here to learn more about Next.js</p>
-          </a>
-          <a href='https://blog.hubspot.com/website/html' className='card'>
-            <h3>HTML and CSS &rarr;</h3>
-            <p>Click here to learn more about HTML and CSS</p>
-          </a>{' '}
-          <a href='https://tailwindcss.com/' className='card'>
-            <h3>Tailwind css&rarr;</h3>
-            <p>Click here to learn more about tailwind css</p>
-          </a>
-          <a href='https://styled-components.com/' className='card'>
-            <h3>Styled Components&rarr;</h3>
-            <p>Click here to learn more about Styled Components</p>
           </a>
         </div>
       </main>
